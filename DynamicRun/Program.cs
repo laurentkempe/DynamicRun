b65f51d0d2c88cc18d47b8a841e7a1b0ad10fe21
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2019-01-15 20:06:57 +0100

    Use ObservableFileSystemWatcher to watch a Sources folder for changes

--- DynamicRun/Program.cs
+++ DynamicRun/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DynamicRun.Builder;
+using System;
+using System.IO;
+using System.Reactive.Linq;
 
 namespace DynamicRun
 {
@@ -6,7 +9,23 @@ namespace DynamicRun
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var sourcesPath = Path.Combine(Environment.CurrentDirectory, "Sources");
+
+            Console.WriteLine($"Running from: {Environment.CurrentDirectory}");
+            Console.WriteLine($"Sources from: {sourcesPath}");
+            Console.WriteLine("Modify the sources to compile and run it!");
+
+            using (var watcher = new ObservableFileSystemWatcher(c => { c.Path = @".\Sources"; }))
+            {
+                var changes = watcher.Changed.Throttle(TimeSpan.FromSeconds(.5)).Where(c => c.FullPath.EndsWith(@"Program.cs")).Select(c => c.FullPath);
+
+                changes.Subscribe(file => Console.WriteLine($"Modidified file: {file}"));
+
+                watcher.Start();
+
+                Console.WriteLine("Press any key to exit!");
+                Console.ReadLine();
+            }
         }
     }
 }
