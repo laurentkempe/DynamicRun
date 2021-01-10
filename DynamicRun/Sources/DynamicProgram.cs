15462eb0cda8b09387e11455a8a05b63ab15df64
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2021-01-10 10:34:10 +0100

    Update Compiler to compile C# 9
    and use C# 9 top-level statements in the DynamicProgram

--- DynamicRun/Sources/DynamicProgram.cs
+++ DynamicRun/Sources/DynamicProgram.cs
@@ -1,12 +1,3 @@
 ﻿using System;
 
-namespace Hello
-{
-    class DynamicProgram
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine($"Hello World from {args[0]}!");
-        }
-    }
-}
+Console.WriteLine($"Hello World from {args[0]}!");
