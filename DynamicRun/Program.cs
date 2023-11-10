7ff242f0ed1e92440d59353bcc6491116af08bfa
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2023-11-10 13:51:51 +0100

    Use regular interpolation adn regular string

--- DynamicRun/Program.cs
+++ DynamicRun/Program.cs
@@ -12,8 +12,8 @@ Console.WriteLine("Modify the sources to compile and run it!");
 var compiler = new Compiler();
 var runner = new Runner();
 
-using var watcher = new ObservableFileSystemWatcher(c => { c.Path = @$".{Path.DirectorySeparatorChar}Sources"; });
-var changes = watcher.Changed.Throttle(TimeSpan.FromSeconds(.5)).Where(c => c.FullPath.EndsWith(@"DynamicProgram.cs")).Select(c => c.FullPath);
+using var watcher = new ObservableFileSystemWatcher(c => { c.Path = $".{Path.DirectorySeparatorChar}Sources"; });
+var changes = watcher.Changed.Throttle(TimeSpan.FromSeconds(.5)).Where(c => c.FullPath.EndsWith("DynamicProgram.cs")).Select(c => c.FullPath);
 
 changes.Subscribe(filepath => runner.Execute(compiler.Compile(filepath), new[] { "France" }));
 
