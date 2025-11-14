6a79d3c7a886527b6560d030e4669d34547d5908
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2025-11-14 20:27:53 +0100

    Update to .NET 10 and C# 14 (#6)
    * Update to .NET 10 and C# 14
    
    Update nugets
    
    * Fix typo in property name: `Word` → `World` in `DynamicProgram.cs`

--- DynamicRun/Sources/DynamicProgram.cs
+++ DynamicRun/Sources/DynamicProgram.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 
-const string s = $"Hello{" "} World"; // C# 10 Constant Interpolated Strings
-var hello = $"{s} from {args[0]}!";
+var hello = $"{Hello.World} from {args[0]}!";
     
 Console.WriteLine(hello);
+
+static class Hello
+{
+    public static string World => field ??= "Hello World"; // C# 14 field keyword
+}
