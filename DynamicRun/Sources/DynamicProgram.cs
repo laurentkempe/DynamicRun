63869666a3525808ac92b06d70da63cf296c67cb
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2025-11-14 20:26:41 +0100

    Fix typo in property name: `Word` → `World` in `DynamicProgram.cs`

--- DynamicRun/Sources/DynamicProgram.cs
+++ DynamicRun/Sources/DynamicProgram.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 
-var hello = $"{Hello.Word} from {args[0]}!";
+var hello = $"{Hello.World} from {args[0]}!";
     
 Console.WriteLine(hello);
 
 static class Hello
 {
-    public static string Word => field ??= "Hello World"; // C# 14 field keyword
+    public static string World => field ??= "Hello World"; // C# 14 field keyword
 }
